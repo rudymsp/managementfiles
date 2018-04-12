371ff0341d9b213a6062b4380ef894836499520e
unknown <IT@IT-PC.MATCHDOMAIN.COM>
2018-04-12 08:35:29 +0700

    kedua

--- buat_folder.php
+++ buat_folder.php
@@ -1,6 +1,5 @@
 <?php
    $folder=$_POST["alamat_folder"];
-   $url=$_POST["alamat_url"];
    if ( is_dir($folder."/".$_POST["nama_folder"])) {
    	  echo "<script> alert('Folder Sudah ada !');history.go(-1);</script>";
    } else {
